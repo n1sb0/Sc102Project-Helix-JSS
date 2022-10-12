1588295cd2ac561bdb2cd66744ea9c624a408164
Bohdan_Sivak <bohdan.sivak@agictech.com>
2022-10-12 16:03:07 +0000

    Added Feature Basic Conent: Section Header Component

---
+++ src/Feature/BasicContent/rendering/Models/SectionHeader.cs
@@ -0,0 +1,9 @@
+﻿using Sitecore.LayoutService.Client.Response.Model.Fields;
+
+namespace Sc102Project.Feature.BasicContent.Models
+{
+    internal class SectionHeader
+    {
+        public TextField Text { get; set; }
+    }
+}
